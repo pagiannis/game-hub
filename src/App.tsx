1044a18bf2b3104658c1ecacbae10c22e14f2606
pagiannis <pagiannis04@gmail.com>
2025-03-20 23:19:35 +0200

    Fetch the genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   const showAside = useBreakpointValue({ base: "none", lg: "block" });
@@ -16,7 +17,7 @@ function App() {
         <NavBar />
       </GridItem>
       <GridItem area="aside" display={showAside}>
-        Aside
+        <GenreList />
       </GridItem>
       <GridItem area="main">
         <GameGrid />
