0cfe476979872cb647dedb6bac0555808aee93ce
pagiannis <pagiannis04@gmail.com>
2025-03-28 16:48:10 +0200

    Implement searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.webp";
 import { ColorModeButton } from "./ui/color-mode";
 import SearchInput from "./SearchInput";
 
-function NavBar() {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+function NavBar({ onSearch }: Props) {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeButton />
     </HStack>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,11 +1,25 @@
 import { Input, InputGroup } from "@chakra-ui/react";
+import { useRef } from "react";
 import { LuSearch } from "react-icons/lu";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <InputGroup flex="1" startElement={<LuSearch />}>
-      <Input borderRadius={20} placeholder="Search games..." />
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup flex="1" startElement={<LuSearch />}>
+        <Input ref={ref} borderRadius={20} placeholder="Search games..." />
+      </InputGroup>
+    </form>
   );
 };
 
